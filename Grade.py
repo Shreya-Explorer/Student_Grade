28b472ac71890a4cf6810c3fa6f154df9f698435
Shreya Raykar <shreyaraikar2304@gmail.com>
2025-11-27 14:03:28 +0530

    Implement grade calculation based on input marks
    Calculates average and assigns grade based on marks.

---
+++ Grade.py
@@ -0,0 +1,36 @@
+import sys
+if len(sys.argv) == 6:
+  script_name = sys.argv[0]
+  m1=sys.argv[1]
+  m2=sys.argv[2]
+  m3=sys.argv[3]
+  m4=sys.argv[4]
+  m5=sys.argv[5]
+else:
+  script_name = sys.argv[0]
+  m1=90
+  m2=86
+  m3=95
+  m4=88
+  m5=70
+avg=(m1+m2+m3+m4+m5)/5
+if(avg>=90):
+  grade='A'
+elif(avg>=80):
+  grade='B'
+elif(avg>=70):
+  grade='C'
+elif(avg>=60):
+  grade='D'
+elif(avg>=50):
+  grade='E'
+else:
+  grade='FAIL'
+print("Marks 1=",m1)
+print("Marks 2=",m2)
+print("Marks 3=",m3)
+print("Marks 4=",m4)
+print("Marks 5=",m5)
+print("Average =",avg)
+print("Grade =",grade)
+  
